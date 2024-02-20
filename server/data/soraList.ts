eb826470455fbab1da5c1a26054786edb461042e
justin3go <just@justin3go.com>
2024-02-20 09:49:43 +0800

    feat: add more sora video

--- server/data/soraList.ts
+++ server/data/soraList.ts
@@ -54,5 +54,152 @@ export const soraData = [
     "homepage": "https://www.youtube.com/@SoraVideoAI",
     "video": "https://www.youtube.com/embed/ubdgBbvJl6E?si=viFbPrtVLqtpdgYZ",
     "publish": "2024-02-16"
-  }
+  },
+  {
+    "prompt": "A young man at his 20s is sitting on a piece of cloud in the sky, reading a book.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/LMB9aVDaArM?si=nqf6M6EQUW9uYgik",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "A close up view of a glass sphere that has a zen garden within it. There is a small dwarf in the sphere who is raking the zen garden and creating patterns in the sand.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/UNCR1HHdtXM?si=aCYcJMffXScB9x3m",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "A cat waking up its sleeping owner demanding breakfast. The owner tries to ignore the cat, but the cat tries new tactics and finally the owner pulls out a secret stash of treats from under the pillow to hold the cat off a little longer.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/0b-NTHdKvgg?si=imwclw-635ZCeOOF",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "A drone camera circles around a beautiful historic church built on a rocky outcropping along the Amalfi Coast, the view showcases historic and magnificent architectural details and tiered pathways and patios, waves are seen crashing against the rocks below as the view overlooks the horizon of the coastal waters and hilly landscapes of the Amalfi Coast Italy, several distant people are seen walking and enjoying vistas on patios of the dramatic ocean views, the warm glow of the afternoon sun creates a magical and romantic feeling to the scene, the view is stunning captured with beautiful photography.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/YrnXiB8FBAo?si=DyxyARWAoMU78Vun",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "Beautiful, snowy Tokyo city is bustling. The camera moves through the bustling city street, following several people enjoying the beautiful snowy weather and shopping at nearby stalls. Gorgeous sakura petals are flying through the wind along with snowflakes.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/bVwiTQ58_7s?si=-bhivuFwZNEtOrKC",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "A instructional cooking session for homemade gnocchi hosted by a grandmother social media influencer set in a rustic Tuscan country kitchen with cinematic lighting",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/1WXMJHYono8?si=4z_ABKsnLzAszFt-",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "Reflections in the window of a train traveling through the Tokyo suburbs.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/g0AMxYSOduI?si=GBZU5GOUKJhSYznH",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "a monkey playing chess in a park.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/JQwVmz7KQHY?si=28MBTNkurLbsojwX",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "Music Video(NOT FOUND)",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/ldHXUv8lzIk?si=Y1E-clLAVSr43qRS",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "Eye of the Tiger Zoo edit ft King Monkey, Tired Tiger, and Snapping Turtle.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/juX1iZIJeUk?si=2hZYAqQ9lC7H2Gt8",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "A bicycle race on ocean with different animals as athletes riding the bicycles with drone camera view.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/3Doxiv1bTEw?si=zWfWVjBl4oLeP3Qy",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "Two golden retrievers podcasting on top of a mountain.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/UqX-BQKNQe0?si=MCSS5KtoqK2ZaUxn",
+    "publish": "2024-02-16"
+  },
+  {
+    "prompt": "pov footage of an ant navigating the inside of an ant nest.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/9Ih9H_yFIz4?si=whhINvXFIIGGr9Td",
+    "publish": "2024-02-19"
+  },
+  {
+    "prompt": "a giant cathedral is completely filled with cats. there are cats everywhere you look. a man enters the cathedral and bows before the giant cat king sitting on a throne.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/JB5LO3cMj-A?si=vhd4ZvfN2mfCEDuI",
+    "publish": "2024-02-19"
+  },
+  {
+    "prompt": "a photorealistic video of a butterfly that can swim navigating underwater through a beautiful coral reef",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/ct61-bJMF3E?si=cZ_q9m--7EX0GA9E",
+    "publish": "2024-02-19"
+  },
+  {
+    "prompt": "A litter of golden retriever puppies playing in the snow. Their heads pop out of the snow, covered in.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/A2XefqGM0Jo?si=H3GiKd2_BWWis8Ev",
+    "publish": "2024-02-20"
+  },
+  {
+    "prompt": "Borneo wildlife on the Kinabatangan River",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/hAGYWpduJ7o?si=6ka9zCiiA2DkMNHQ",
+    "publish": "2024-02-20"
+  },
+  {
+    "prompt": "A petri dish with a bamboo forest growing within it that has tiny red pandas running around.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/v3qTu6WoHgs?si=fRyGQ0hriVAfvAhp",
+    "publish": "2024-02-20"
+  },
+  {
+    "prompt": "Asian Winter(NOT FOUND)",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/6T2BA3yCYFQ?si=iWeZmuK_1pwQ5Gyv",
+    "publish": "2024-02-20"
+  },
+  {
+    "prompt": "The story of a robot’s life in a cyberpunk setting.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/c-2Ih7cQiFI?si=3kEVmovBuenWYY6B",
+    "publish": "2024-02-20"
+  },
+  {
+    "prompt": "The camera follows behind a white vintage SUV with a black roof rack as it speeds up a steep dirt road surrounded by pine trees on a steep mountain slope, dust kicks up from it’s tires, the sunlight shines on the SUV as it speeds along the dirt road, casting a warm glow over the scene. The dirt road curves gently into the distance, with no other cars or vehicles in sight. The trees on either side of the road are redwoods, with patches of greenery scattered throughout. The car is seen from the rear following the curve with ease, making it seem as if it is on a rugged drive through the rugged terrain. The dirt road itself is surrounded by steep hills and mountains, with a clear blue sky above with wispy clouds.",
+    "author": "openaisora",
+    "homepage": "https://www.youtube.com/@SoraVideoAI",
+    "video": "https://www.youtube.com/embed/NZwb5DMyLgU?si=JUd_aBoUueYpFT8b",
+    "publish": "2024-02-20"
+  },
 ]

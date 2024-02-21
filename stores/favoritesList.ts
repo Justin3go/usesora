9be90decd7c7580afc932f848f14d37c5470dce1
justin3go <just@justin3go.com>
2024-02-21 14:16:04 +0800

    feat: add star col to support favorite

--- stores/favoritesList.ts
+++ stores/favoritesList.ts
@@ -1,11 +1,34 @@
 import { defineStore } from 'pinia'
 
+interface IFavoritesListItem {
+  prompt: string;
+  author: string;
+  homepage: string;
+  video: string;
+  publish: string;
+}
+
 export const useMyFavoritesListStore = defineStore({
   id: 'myFavoritesListStore',
   state: () => ({
-    favoritesList: [],
+    favoritesList: [] as IFavoritesListItem[],
   }),
-  actions: {},
+  actions: {
+    addFavorite(item: IFavoritesListItem) {
+      const index = this.favoritesList.findIndex((i) => i.prompt === item.prompt)
+      if (index === -1) {
+        this.favoritesList.push(item)
+        return true;
+      }
+      else return false;
+    },
+    removeFavorite(item: IFavoritesListItem) {
+      const index = this.favoritesList.findIndex((i) => i.prompt === item.prompt)
+      if (index !== -1) {
+        this.favoritesList.splice(index, 1)
+      }
+    },
+  },
   persist: {
     storage: persistedState.localStorage,
   },
